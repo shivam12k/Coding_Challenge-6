250c0ee8d484815a2f94ca372aea956ce6f25b08
shivam <mynameisshivam78@gmail.com>
2020-05-26 22:24:05 +0530

    adding file

---
+++ question-one.js
@@ -0,0 +1,12 @@
+for(i=0; i<=5;i++)
+{
+var input= document.querySelector('.input')
+}
+/*var sort=input.sort(function(a,b){
+	return a-b;
+});
+console.log(arr); 
+*/
+console.log(input);
+
+/*var arr=[1,2,4,591,392,391,2,5,5,2,1,1,1,20,20]*/
