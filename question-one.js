4f652c65914ebb148aa6c6548a06b6ed6f40ae75
shivam <mynameisshivam78@gmail.com>
2020-05-27 12:52:15 +0530

    takes user input and sort

--- question-one.js
+++ question-one.js
@@ -1,12 +1,18 @@
-for(i=0; i<=5;i++)
-{
-var input= document.querySelector('.input')
-}
-/*var sort=input.sort(function(a,b){
-	return a-b;
-});
-console.log(arr); 
-*/
-console.log(input);
+var x=0;
+var arr=[]
+let input=[]
 
-/*var arr=[1,2,4,591,392,391,2,5,5,2,1,1,1,20,20]*/
+const take=()=>{
+	arr[x]=document.querySelector(".input").value;
+	document.querySelector(".input").value="";
+	input.push(arr[x]);
+	x++;
+
+}
+
+const done=()=>{
+	input.sort(function(a,b){
+		return a-b;
+	})
+	console.log(input);
+}
